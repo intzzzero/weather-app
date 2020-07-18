1c17600e11af9aee36ff4d49a6a197965da6cd8e
jeongsooyoung <code.ameba@gmail.com>
2020-07-18 19:06:23 +0900

    practice state

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default function App() {
+const App = () => {
+	const [ inputText, setInputText ] = useState('');
+
 	return (
 		<View style={styles.container}>
-			<Text style={styles.text}>codeAmeba</Text>
-			<StatusBar style='auto' />
+			<Text style={styles.text}>{inputText}</Text>
+			<TextInput onChangeText={inputText => setInputText(inputText)} style={styles.input} />
 		</View>
 	);
-}
+};
 
 const styles = StyleSheet.create({
 	container: {
@@ -22,9 +24,18 @@ const styles = StyleSheet.create({
 		color: 'rgba(0, 0, 0, 0.8)',
 		fontSize: 35,
 		fontWeight: '600',
-		padding: 10,
-		borderWidth: 3,
-		borderRadius: 15,
-		borderColor: 'rgba(0, 0, 0, 0.8)'
+		padding: 10
+	},
+	input: {
+		height: 20,
+		width: 150,
+		fontSize: 20,
+		textAlign: 'center',
+		color: 'rgba(0, 0, 0, 0.8)',
+		borderBottomColor: 'rgba(0, 0, 0, 0.8)',
+		borderBottomWidth: 3,
+		marginTop: 30
 	}
 });
+
+export default App;
